22554af075707cf44281d4f3d2e9619a722af0bc
Adam Roberts <aroberts85@yahoo.com>
2017-07-13 15:04:20 -0400

    front end add

--- flashcards-client/src/App.js
+++ flashcards-client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import Button from 'react-toolbox/lib/button/Button';
+import { getQueryParams } from './utils';
+import Login from './Login';
+import Main from './Main';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    const params = getQueryParams();
+    this.state = { token: params.token };
+  }
+
+  isLoggedIn() {
+    return !!this.state.token;
+  }
+
   render() {
     return (
-      <div className="App">
-        <Button raised primary>
-          Hi!
-        </Button>
+      <div className='App'>
+        {this.isLoggedIn()
+          ? <Main token={this.state.token} />
+          : <Login />
+        }
       </div>
     );
   }
--- flashcards-client/src/App.test.js
+++ flashcards-client/src/App.test.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import Login from './Login';
+import Main from './Main';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  let wrapper;
+
+  describe('without a token param', () => {
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+    });
+
+    it('renders a Login view', () => {
+      expect(wrapper.find(Login)).toHaveLength(1);
+    });
+
+    it('does not render a Main view', () => {
+      expect(wrapper.find(Main)).toHaveLength(0);
+    });
+  });
+
+  describe('with a token param', () => {
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+      wrapper.setState({token: 'foobar'});
+    });
+
+    it('does not render a Login view', () => {
+      expect(wrapper.find(Login)).toHaveLength(0);
+    });
+
+    it('renders a Main view', () => {
+      expect(wrapper.find(Main)).toHaveLength(1);
+    });
+  });
 });
